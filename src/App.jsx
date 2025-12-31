d9851aecb30a1e0d561437d7de30a94707ceab9c
JoseNavarroMiranda <nqvarr01234@gmail.com>
2025-12-30 21:00:44 -0600

    Adding of components and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/static/navbar';
+import Information from './components/information/information';
+import Footer from './components/static/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Navbar />
+      <Information/>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/information/information.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Information(){
+
+    return <p>Aqui es donde se mostrara la informacion</p>
+}
