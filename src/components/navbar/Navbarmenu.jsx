777c0ec67c36893c5a9be892d820d7baa947e1e7
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-01-05 23:12:23 -0600

    modification in navbar

--- src/components/navbar/Navbarmenu.jsx
+++ src/components/navbar/Navbarmenu.jsx
@@ -42,7 +42,7 @@ function Navbarmenu() {
                 <a
                   key={label}
                   href={`#${id}`}
-                  className="px-3 py-2 text-sm font-medium rounded-md text-white/80 transition
+                  className="px-5 py-5 text-l font-medium rounded-md text-white/150 transition
                              hover:text-white hover:bg-white/10
                              focus:outline-none focus:ring-2 focus:ring-fuchsia-300/30">
                   {label}
