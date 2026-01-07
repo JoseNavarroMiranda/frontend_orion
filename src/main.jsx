9b401a7ee14317443fe747d52084cb0f292d034a
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-01-07 12:01:31 -0600

    interation in footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import Navbarmenu from './components/navbar/Navbarmenu.jsx'
 import MainInfo from './components/information/stack/MainInfo.jsx'
 import MiddleInfo from './components/information/stack/MiddleInfo.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbarmenu />
     <MainInfo />
     <MiddleInfo />
+    <Footer />
   </StrictMode>
 )
